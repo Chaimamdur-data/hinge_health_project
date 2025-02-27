1f7dd8075064d84946554467675ab6929fc5edf8
CMamdur <mchaitanya@gmail.com>
2025-02-27 10:21:33 -0800

    Update mart_cleaned_members.sql

--- models/mart/mart_cleaned_members.sql
+++ models/mart/mart_cleaned_members.sql
@@ -4,15 +4,13 @@
     unique_key='id'
 ) }}
 
-WITH suspicious_records AS (
-    SELECT * FROM {{ ref('core_combined_members') }}
-    WHERE joined_year < YEAR(dob)  
-    OR last_active < dob
+
 )
 
 SELECT * FROM {{ ref('core_combined_members') }}
-WHERE id NOT IN (SELECT id FROM suspicious_records)
-
+WHERE joined_year < YEAR(dob)  
+    OR last_active < dob
+    
 {% if is_incremental() %}
 AND last_active > (SELECT MAX(last_active) FROM {{ this }})
 {% endif %};
