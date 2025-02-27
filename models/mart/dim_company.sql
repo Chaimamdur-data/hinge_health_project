3d224bea4a7d66c2a12d8286db365134eabd1d27
CMamdur <mchaitanya@gmail.com>
2025-02-27 10:18:59 -0800

    Update dim_company.sql

--- models/mart/dim_company.sql
+++ models/mart/dim_company.sql
@@ -1,8 +1,13 @@
-{{ config(materialized='table') }}
+{{ config(materialized="table") }}
 
-SELECT 
-    {{ dbt_utils.generate_surrogate_key(['id']) }} AS company_sk,
-    id AS natural_company_id,
-    name AS company_name
-FROM {{ ref('stg_companies') }}
+
+SELECT distinct
+    HASH(
+        COALESCE(CAST(c.company_id AS STRING), '_dbt_utils_surrogate_key_null_')
+    ) AS company_sk,
+    c.company_id AS company_id,
+   case when s.company_id is null then 'company name not defined' else s.company_id AS company_name
+FROM {{ ref('core_combined_members') }} c  
+LEFT JOIN {{ ref('stg_companies') }} s
+ON c.company_id = s.company_id
 
