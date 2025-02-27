a1c81d1bc1cae46cbbff5a799ef2793dc368ebd5
CMamdur <mchaitanya@gmail.com>
2025-02-27 10:19:19 -0800

    Update dim_location.sql

--- models/mart/dim_location.sql
+++ models/mart/dim_location.sql
@@ -2,11 +2,5 @@
 
 SELECT 
     {{ dbt_utils.generate_surrogate_key(['state']) }} AS location_sk,
-    state,
-    CASE 
-        WHEN state IN ('CA', 'OR', 'WA') THEN 'West'
-        WHEN state IN ('NY', 'NJ', 'MA') THEN 'East'
-        ELSE 'Other'
-    END AS region
+    state
 FROM {{ ref('core_combined_members') }}
-
