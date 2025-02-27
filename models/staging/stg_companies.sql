0de83057be1aa3c4958c5f02e8a3a0be7bf43ac2
CMamdur <mchaitanya@gmail.com>
2025-02-27 10:24:44 -0800

    Update stg_companies.sql

--- models/staging/stg_companies.sql
+++ models/staging/stg_companies.sql
@@ -1,17 +1,7 @@
-{{ config(
-    materialized='view'
-) }}
 WITH source AS (
     SELECT
-        id,
-        first_name,
-        last_name,
-        CAST(dob AS DATE) AS dob,
-        company_id,
-        CAST(last_active AS DATE) AS last_active,
-        score,
-        CAST(member_since AS INT) AS joined_year,
-        UPPER(state) AS state
-    FROM {{ ref('raw_unity_golf_club') }}
+        id AS company_id,
+        name AS company_name
+    FROM {{ ref('raw_companies') }}
 )
 SELECT * FROM source
