6d9f25f47358ab697dc8195130123ccac69e9def
CMamdur <mchaitanya@gmail.com>
2025-02-27 10:19:58 -0800

    Update dim_member.sql

--- models/mart/dim_member.sql
+++ models/mart/dim_member.sql
@@ -1,12 +1,10 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    {{ dbt_utils.generate_surrogate_key(['id']) }} AS member_sk,
-    id AS natural_member_id,
+    id AS member_sk,
+    id AS member_id,
     first_name,
     last_name,
     dob,
     state
 FROM {{ ref('core_combined_members') }}
-
-
